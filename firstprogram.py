e47fa2280a711ddf1ee70823d4e094f7290dfebe
Anil <anilkumarmalla8@gmail.com>
2025-04-30 23:16:37 +0545

    Initial commit

---
+++ firstprogram.py
@@ -0,0 +1,11 @@
+print ("hello world.", "I am Anil MAlla")
+print ("I am Anil MAlla")
+print (23+35)
+
+name = "anil" #string 
+age = 23 # integer
+price = 29.7 # floating value 
+
+print("My name is : ", name)
+print ("My age is : ", age)
+
